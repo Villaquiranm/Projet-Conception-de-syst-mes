6e7a19509905af443f3ef71d17a8879b9394dc70
Miguel Alfredo Victoria <victorim@ensipc106.imag.fr>
2017-11-13 11:35:58 +0100

    In this séance, I started with the first part of SEOC Work, We are ready with Multi-task Processes

--- src_de_base/processus.c
+++ src_de_base/processus.c
@@ -20,52 +20,74 @@ struct processus {
     struct reg_type zone;
     uint32_t pile[512];
 };
-struct processus t_Processus[2];
+int32_t actif=0;
+int32_t avant=1;
+#define _PROCESS 4
+
+struct processus t_Processus[_PROCESS];
+char *mon_nom(){
+  return t_Processus[actif].nom;
+}
+uint32_t mon_pid(){
+  return t_Processus[actif].pid;
+}
+void ordonnance(){
+        printf("[%s] je donne la main pid=%i \n",mon_nom(),mon_pid());
+        t_Processus[actif].etat=PRETE;
+        t_Processus[actif+1].etat=ELU;
+        if (actif==_PROCESS-1){
+          actif=0;
+          avant=_PROCESS-1;
+        }else{
+          avant=actif;
+          actif++;
+
+        }
+        ctx_sw(&t_Processus[avant].zone.ebx,&t_Processus[actif].zone.ebx);
+
+    }
+
 void idle(void){
     for (int i=0;i<3;i++){
-        printf("[idle] je tente de passer la main a proc1...\n");
+        /*printf("[idle] je tente de passer la main a proc1...\n");
         ctx_sw(&t_Processus[0].zone.ebx,&t_Processus[1].zone.ebx);
-        printf("[idle] proc1 m'a redonne la main \n");
+        printf("[idle] proc1 m'a redonne la main \n");*/
+        ordonnance();
     }
     printf("[idle] je bloque le systeme\n");
     hlt();
 }
 void proc1(void){
-    for(int a=0; a<3;a++){
-        printf("[proc1] idle m'a donne la main \n");
-        printf("[proc1] je tente de lui la redonner] \n");
-        ctx_sw(&t_Processus[1].zone.ebx,&t_Processus[0].zone.ebx);
+    for(int i=0;i<3;i++){
+        ordonnance();
     }
-     
-       
+}
+void proc2(void){
+    for(int i=0;i<3;i++){
+        ordonnance();
+    }
+}
+void proc3(void){
+    for(int i=0;i<3;i++){
+        ordonnance();
+    }
 }
 void init(){
     sprintf(t_Processus[0].nom,"Idle");
-    sprintf(t_Processus[1].nom,"proc1");
     t_Processus[0].pid=0;
-    t_Processus[1].pid=1;
-    t_Processus[0].etat=ELU;
-    t_Processus[1].etat=PRETE;
-    t_Processus[1].zone.esp=(uint32_t)(&t_Processus[1].pile[511]);
-    t_Processus[1].pile[511]=(uint32_t)(&proc1);   
-}
-void ordonnance(){
-  
-    if (t_Processus[0].etat==ELU){
-        t_Processus[0].etat=PRETE;
-        t_Processus[1].etat=ELU;
-        printf("[idle] je donne la main \n");
-        ctx_sw(&t_Processus[0].zone.ebx,&t_Processus[1].zone.ebx);
+    for (size_t i = 1; i < _PROCESS; i++) {
+      t_Processus[i].pid=i;
+      sprintf(t_Processus[i].nom,"proc%i",i);
+      t_Processus[i].etat=PRETE;
+      t_Processus[i].zone.esp=(uint32_t)(&t_Processus[i].pile[511]);
     }
-    else{
-        t_Processus[1].etat=PRETE;
-        t_Processus[0].etat=ELU;
-        printf("[proc1] Je donne la main \n");
-        ctx_sw(&t_Processus[1].zone.ebx,&t_Processus[0].zone.ebx);
-
-    }   
+    t_Processus[0].etat=ELU;
+    t_Processus[1].pile[511]=(uint32_t)(&proc1);
+    t_Processus[2].pile[511]=(uint32_t)(&proc2);
+    t_Processus[3].pile[511]=(uint32_t)(&proc3);
 }
-char *mon_nom(void){
+
+/*char *mon_nom(void){
     int i=0;
     while (t_Processus[i].etat==PRETE){
         i++;
@@ -78,4 +100,4 @@ int mon_pid(void){
         i++;
     }
     return t_Processus[i].pid;
-}
+}*/
