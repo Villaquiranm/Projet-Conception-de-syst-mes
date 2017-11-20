36e4bbce81c5513307ec60de7e664a56e7dd56ef
Miguel Alfredo Victoria <victorim@ensipc332.imag.fr>
2017-11-20 11:17:43 +0100

    We finished the project maybe we can add some colors but i dont want to.

--- src_de_base/processus.c
+++ src_de_base/processus.c
@@ -70,33 +70,40 @@ void idle(void){
 }
 void fin_processus(){
   t_Processus[actif].etat=MORT;
-  printf("[%i] %s est mort maintenant\n",actif,mon_nom());
+  printf("[%i]\t\t\t%s\t\tEst mort maintenant\n",actif,mon_nom());
   ordonnance();
 }
 int32_t cree_processus(void (*code)(void), char *nom){
   int actual_process=na_processes;
-  for (size_t i = 1; i < na_processes; i++) {
+  //Regarder s'il y a des processes morts, et s'il y en a, on peut utiliser ce pid.
+  for (size_t i = 1; i <= na_processes; i++) {
     if (t_Processus[i].etat==MORT){
       actual_process=i;
       break;
     }
   }
+  //Si on n'a pas trouvé un processus MORT et on a dépasé le nombre maximum de processus
+  //il faut renvoyer -1 (Erreur)
   if (na_processes==_PROCESS-1 && actual_process==na_processes){
+    printf("[Erreur]\t\t[Plein]\t\tNombre de processus plein\n");
     return -1;
   }
+  //Sinon on peur continuer avec la creation du processus
   else{
+    //Si on n'a pas trouve un processus MORT on doit augmenter le nombre de processus
+    //Actifs (na_processes)
     if (actual_process==na_processes) {
       na_processes++;
       actual_process=na_processes;
     }
-
     t_Processus[actual_process].pid=actual_process;
     sprintf(t_Processus[actual_process].nom,"%s",nom);
     t_Processus[actual_process].zone.esp=(uint32_t)(&t_Processus[actual_process].pile[510]);
     t_Processus[actual_process].pile[510]=(uint32_t)(code);
     t_Processus[actual_process].pile[511]=(uint32_t)(&fin_processus);
-    t_Processus[actual_process].s_reveil=0;
+    t_Processus[actual_process].s_reveil=get_time();
     t_Processus[actual_process].etat=PRETE;
+    printf("[%i]\t\t\t%s\t\tEst ne maintenant\n",actual_process,t_Processus[actual_process].nom);
   return 0;
   }
 }
@@ -111,26 +118,24 @@ void proc1(void){
 
     for(int i=0;i<5;i++){
       //t_Processus[actif].pile[511]=(uint32_t)(&fin_processus);
-      printf("[temps = %u] processus %s pid = %i\n", get_time(),mon_nom(), mon_pid());
+      printf("[temps = %u]\t\t%s\t\tpid = %i\n", get_time(),mon_nom(), mon_pid());
       dors(1);
     }
     cree_processus(&proc1,"proc1");
 }
 void proc2(void){
     for(int i=0;i<5;i++){
-      printf("[temps = %u] processus %s pid = %i\n", get_time(),mon_nom(), mon_pid());
+      printf("[temps = %u]\t\t%s\t\tpid = %i\n", get_time(),mon_nom(), mon_pid());
       dors(2);
     }
     cree_processus(&proc2,"proc2");
-    //fin_processus();
 }
 void proc3(void){
     for(int i=0;i<5;i++){
-      printf("[temps = %u] processus %s pid = %i\n", get_time(),mon_nom(), mon_pid());
+      printf("[temps = %u]\t\t%s\t\tpid = %i\n", get_time(),mon_nom(), mon_pid());
       dors(3);
     }
     cree_processus(&proc3,"proc3");
-    //fin_processus();
 }
 
 void init(){
@@ -141,5 +146,4 @@ void init(){
     cree_processus(&proc1,"proc1");
     cree_processus(&proc2,"proc2");
     cree_processus(&proc3,"proc3");
-    //cree_processus(&proc3,"proc3");
 }
